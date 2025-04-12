bdb518f170d81409919da34a273bb9e698af36fe
Maxim Krestyaninov <maxipy@ya.ru>
2025-04-12 23:05:59 +0400

    feat: add queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -133,8 +133,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(async (acc, promise) => {
+    const string = await acc;
+    const result = await promise;
+    return string + result;
+  }, '');
 }
 
 module.exports = {
